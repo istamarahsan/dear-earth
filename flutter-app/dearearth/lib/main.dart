e6fb55015e16847c5a01fed3d42a0afb02631639
wensen2525 <109505910+wensen2525@users.noreply.github.com>
2024-01-12 21:37:44 +0700

    update explore page

--- flutter-app/dearearth/lib/main.dart
+++ flutter-app/dearearth/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:dearearth/pages/home.dart';
 import 'package:dearearth/pages/login.dart';
-import 'package:dearearth/pages/evaluate.dart';
 import 'package:dearearth/pages/explore.dart';
+import 'package:dearearth/pages/evaluate.dart';
 import 'package:dearearth/pages/profile.dart';
 import 'package:dearearth/models/menu_bar.dart';
 import 'package:pocketbase/pocketbase.dart';
@@ -29,9 +29,9 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
     _pages = [
-      // HomePage(),
+      HomePage(),
       // nanti yang dipakai ini
-      if (isLoggedIn()) HomePage() else LoginPage(pb: widget.pb),
+      // if (isLoggedIn()) HomePage() else LoginPage(pb: widget.pb),
       EvaluatePage(),
       ExplorePage(),
       ProfilePage(pb: widget.pb), // Access widget.pb here
@@ -54,105 +54,104 @@ class _MyAppState extends State<MyApp> {
         ),
         debugShowCheckedModeBanner: false,
         // nanti ini yang dipakai
-        home: isLoggedIn()
-            ? Scaffold(
-                body: _pages[_currentIndex],
-                bottomNavigationBar: NavigationBar(
-                  onDestinationSelected: (int index) {
-                    setState(() {
-                      _currentIndex = index;
-                    });
-                  },
-                  indicatorColor: Colors.amber,
-                  selectedIndex: _currentIndex,
-                  destinations: const <Widget>[
-                    NavigationDestination(
-                      selectedIcon: Icon(Icons.book),
-                      icon: Icon(Icons.book),
-                      label: 'Journal',
-                    ),
-                    NavigationDestination(
-                      icon: Badge(child: Icon(Icons.calculate)),
-                      label: 'Evaluate',
-                    ),
-                    NavigationDestination(
-                      icon: Badge(
-                        label: Text('2'),
-                        child: Icon(Icons.compass_calibration),
-                      ),
-                      label: 'Explore',
-                    ),
-                    NavigationDestination(
-                      icon: Badge(
-                        label: Text('3'),
-                        child: Icon(Icons.portable_wifi_off_outlined),
-                      ),
-                      label: 'Profile',
-                    ),
-                  ],
+        // home: isLoggedIn()
+        //     ? Scaffold(
+        //         body: _pages[_currentIndex],
+        //         bottomNavigationBar: NavigationBar(
+        //           onDestinationSelected: (int index) {
+        //             setState(() {
+        //               _currentIndex = index;
+        //             });
+        //           },
+        //           indicatorColor: Colors.amber,
+        //           selectedIndex: _currentIndex,
+        //           destinations: const <Widget>[
+        //             NavigationDestination(
+        //               selectedIcon: Icon(Icons.book),
+        //               icon: Icon(Icons.book),
+        //               label: 'Journal',
+        //             ),
+        //             NavigationDestination(
+        //               icon: Badge(child: Icon(Icons.calculate)),
+        //               label: 'Evaluate',
+        //             ),
+        //             NavigationDestination(
+        //               icon: Badge(
+        //                 label: Text('2'),
+        //                 child: Icon(Icons.compass_calibration),
+        //               ),
+        //               label: 'Explore',
+        //             ),
+        //             NavigationDestination(
+        //               icon: Badge(
+        //                 label: Text('3'),
+        //                 child: Icon(Icons.portable_wifi_off_outlined),
+        //               ),
+        //               label: 'Profile',
+        //             ),
+        //           ],
+        //         ),
+        //       )
+        //     : LoginPage(pb: widget.pb),
+        home: Scaffold(
+          body: _pages[_currentIndex],
+          bottomNavigationBar: NavigationBar(
+            backgroundColor: Colors.white,
+            onDestinationSelected: (int index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            indicatorColor: Color(0xffDAE7C9),
+            selectedIndex: _currentIndex,
+            destinations: <Widget>[
+              NavigationDestination(
+                selectedIcon: Image.asset(
+                  'assets/icons/journal.png',
+                  height: 24,
+                  width: 24,
                 ),
-              )
-            : LoginPage(pb: widget.pb),
-        // home: Scaffold(
-        //   body: _pages[_currentIndex],
-        //   bottomNavigationBar: NavigationBar(
-        //     backgroundColor: Colors.white,
-        //     onDestinationSelected: (int index) {
-        //       setState(() {
-        //         _currentIndex = index;
-        //       });
-        //     },
-        //     indicatorColor: Color(0xffDAE7C9),
-        //     selectedIndex: _currentIndex,
-        //     destinations: <Widget>[
-        //       NavigationDestination(
-        //         selectedIcon: Image.asset(
-        //           'assets/icons/journal.png',
-        //           height: 24,
-        //           width: 24,
-        //         ),
-        //         icon: Image.asset(
-        //           'assets/icons/journal.png',
-        //           height: 24,
-        //           width: 24,
-        //         ),
-        //         label: 'Journal',
-        //       ),
-        //       NavigationDestination(
-        //         icon: Badge(
-        //           child: Image.asset(
-        //             'assets/icons/evaluate.png',
-        //             height: 24,
-        //             width: 24,
-        //           ),
-        //         ),
-        //         label: 'Evaluate',
-        //       ),
-        //       NavigationDestination(
-        //         icon: Badge(
-        //           label: Text('2'),
-        //           child: Image.asset(
-        //             'assets/icons/compas.png',
-        //             height: 24,
-        //             width: 24,
-        //           ),
-        //         ),
-        //         label: 'Explore',
-        //       ),
-        //       NavigationDestination(
-        //         icon: Badge(
-        //           label: Text('3'),
-        //           child: Image.asset(
-        //             'assets/icons/profile.png',
-        //             height: 24,
-        //             width: 24,
-        //           ),
-        //         ),
-        //         label: 'Profile',
-        //       ),
-        //     ],
-        //   ),
-        // ));
-        );
+                icon: Image.asset(
+                  'assets/icons/journal.png',
+                  height: 24,
+                  width: 24,
+                ),
+                label: 'Journal',
+              ),
+              NavigationDestination(
+                icon: Badge(
+                  child: Image.asset(
+                    'assets/icons/evaluate.png',
+                    height: 24,
+                    width: 24,
+                  ),
+                ),
+                label: 'Evaluate',
+              ),
+              NavigationDestination(
+                icon: Badge(
+                  label: Text('2'),
+                  child: Image.asset(
+                    'assets/icons/compas.png',
+                    height: 24,
+                    width: 24,
+                  ),
+                ),
+                label: 'Explore',
+              ),
+              NavigationDestination(
+                icon: Badge(
+                  label: Text('3'),
+                  child: Image.asset(
+                    'assets/icons/profile.png',
+                    height: 24,
+                    width: 24,
+                  ),
+                ),
+                label: 'Profile',
+              ),
+            ],
+          ),
+        ));
   }
 }
--- flutter-app/dearearth/lib/pages/evaluate.dart
+++ flutter-app/dearearth/lib/pages/evaluate.dart
@@ -1,105 +1,13 @@
 import 'package:flutter/material.dart';
 
 class EvaluatePage extends StatelessWidget {
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-
-  TextEditingController _searchController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      appBar: _appBar(),
-      body: ListView(
-        children: [
-          SizedBox(
-            height: 10,
-          ),
-          _searchField(),
-          SizedBox(
-            height: 30,
-          ),
-          // _headerSection(context),
-          SizedBox(
-            height: 30,
-          ),
-          // _topicsSections(),
-          SizedBox(
-            height: 30,
-          ),
-        ],
+    return Center(
+      child: Text(
+        'Evaluate Page',
+        style: TextStyle(fontSize: 20),
       ),
-    );
-  }
-
-  Widget _searchField() {
-    return TextField(
-      controller: _searchController,
-      decoration: InputDecoration(
-        labelText: "Search",
-        hintText: "Search for something...",
-        prefixIcon: Icon(Icons.search),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-      ),
-      onChanged: (value) {
-        // Handle search input changes here
-        // You can use the value to filter or search data
-        print("Search input: $value");
-      },
-    );
-  }
-
-  AppBar _appBar() {
-    return AppBar(
-      title: Padding(
-        padding: const EdgeInsets.only(left: 8),
-        child: Text(
-          'Co-Link Earthian 🌍🤝',
-          style: TextStyle(
-              color: Color(0xff174A41),
-              fontSize: 22,
-              fontWeight: FontWeight.w600),
-        ),
-      ),
-      backgroundColor: Colors.white,
-      elevation: 0.0,
-      centerTitle: false,
-      actions: [
-        GestureDetector(
-          onTap: () {},
-          child: Container(
-            alignment: Alignment.center,
-            margin: EdgeInsets.only(right: 20, top: 10, bottom: 10),
-            padding: EdgeInsets.only(right: 15, top: 8, bottom: 8, left: 15),
-            decoration: BoxDecoration(
-                color: Color(0xffF8F9FA),
-                borderRadius: BorderRadius.circular(40)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/icons/exp.png',
-                  height: 20,
-                  width: 20,
-                ),
-                SizedBox(
-                  width: 5,
-                ),
-                Text(
-                  '595 xp',
-                  style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.black),
-                )
-              ],
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
